8a4f44cf765499533e2216ed49359caf64f0103d
Raymond Hendy Susanto <raymhs91@gmail.com>
2017-06-06 17:23:12 +0800

    Scripts for installing jnlua

---
+++ install_jnlua/MacOSX/setup.sh
@@ -0,0 +1,11 @@
+CURRENT_DIR=$PWD
+STATNLP_DIR=$PWD/../..
+mkdir -p $STATNLP_DIR/nativeLib
+rm -rf jnlua-0.9.6*
+wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/jnlua/jnlua-0.9.6-src.zip
+unzip jnlua-0.9.6-src.zip
+cp Makefile jnlua-0.9.6/src/main/c/MacOSX/Makefile 
+cd jnlua-0.9.6/src/main/c/MacOSX && make
+cd $CURRENT_DIR
+cp jnlua-0.9.6/src/main/c/MacOSX/libjnlua5.1.jnilib $STATNLP_DIR/nativeLib
+cp $HOME/torch/install/lib/libTH.dylib $STATNLP_DIR/nativeLib
