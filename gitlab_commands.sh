78a2fc6c6b238036b9e15e110b7b6b45f75f717d
RoozbehSanaei <Roozbeh.Sanaei2@gmail.com>
2017-07-20 05:49:53 -0400

    adding files

--- gitlab_commands.sh
+++
@@ -1,8 +0,0 @@
-git remote -v
-git remote add $repository_name https://gitlab.com/sutd_nlp/statnlp-core.git
-
-
-git add *
-git commit -a -m "c based optimization - pass through copy version"
-git push origin optimization_1
-
