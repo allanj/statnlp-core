8d95662b6caf61cf5ff58097f70308eed13d33b1
RoozbehSanaei <Roozbeh.Sanaei2@gmail.com>
2017-07-20 05:28:55 -0400

    optimization - copy

--- gitlab.sh
+++ gitlab.sh
@@ -1,5 +1,5 @@
 git add *
-git commit -a -m "optimization works through copy"
+git commit -a -m "optimization - copy"
 git remote add $repository_name https://gitlab.com/sutd_nlp/statnlp-core.git
 git remote -v
 git push origin optimization_1
