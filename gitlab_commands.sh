68d97912d7e2d6d73e0d52f2ae1c204d5ad2d765
RoozbehSanaei <Roozbeh.Sanaei2@gmail.com>
2017-07-20 05:33:39 -0400

    c based optimization - pass through copy version

--- gitlab_commands.sh
+++ gitlab_commands.sh
@@ -1,6 +1,8 @@
+git remote -v
+git remote add $repository_name https://gitlab.com/sutd_nlp/statnlp-core.git
+
+
 git add *
 git commit -a -m "c based optimization - pass through copy version"
-git remote add $repository_name https://gitlab.com/sutd_nlp/statnlp-core.git
-git remote -v
 git push origin optimization_1
 
